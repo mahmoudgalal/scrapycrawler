48f21cade0851f2c20362463a0feb2123aee4565
mahmoudgalal <mahmoudgalal57@yahoo.com>
2017-04-25 15:21:52 +0200

    removing all unneeded data

--- TestSpider/spiders/test_spider.py
+++ TestSpider/spiders/test_spider.py
@@ -5,15 +5,17 @@ import scrapy
 import datetime
 import time
 import os
+import StringIO
+import pkgutil
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from TestSpider.items import TestspiderItem
 class TestSpider(CrawlSpider):
     name = 'pages'
     #put here all the urls for data sources
-    #start_urls = ['http://www.foodnavigator.com',
-    #       'http://foodnewsinternational.com' ]
-    start_urls = []
+    start_urls = ['http://www.foodnavigator.com',
+           'http://foodnewsinternational.com' ]
+    #start_urls = []
     rules = ( Rule(LinkExtractor(), callback='parse_item' ),)
     # def parse_item(self,response):
         # self.logger.info('Hi, this is an item page! %s', response.url)
@@ -24,10 +26,13 @@ class TestSpider(CrawlSpider):
         # self.log('Saved file %s' % filename)
     def __init__(self, *args, **kwargs):
         super(TestSpider, self).__init__(*args, **kwargs)
-        with open(os.path.join(os.path.dirname(__file__), 'urllist.txt')) as file:
-          for line in file:
-              line = line.strip() #or someother preprocessing
-              self.start_urls.append(line)
+        #stringinput  = pkgutil.get_data("scrapycrawler", "resources/urllist.txt")
+        #for line in StringIO.StringIO(stringinput):
+         # self.start_urls.append(line)
+        #with open(os.path.join(os.path.dirname(__file__), 'urllist.txt'),'r') as file:
+          #for line in file:
+        #      line = line.strip() #or someother preprocessing
+         #     self.start_urls.append(line)
     def parse_item(self,response):
         self.logger.info('Hi, this is an item page! %s', response.url)
         page = int(round(time.time() * 1000))
