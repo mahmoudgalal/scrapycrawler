d971a6367c8b35c420ac04ecf3395642171ca83f
mahmoudgalal <mahmoudgalal57@yahoo.com>
2017-04-25 14:24:07 +0200

    add standlone text fie for all links

--- TestSpider/spiders/test_spider.py
+++ TestSpider/spiders/test_spider.py
@@ -10,8 +10,9 @@ from TestSpider.items import TestspiderItem
 class TestSpider(CrawlSpider):
     name = 'pages'
     #put here all the urls for data sources
-    start_urls = ['http://www.foodnavigator.com',
-            'http://foodnewsinternational.com' ]
+    #start_urls = ['http://www.foodnavigator.com',
+    #       'http://foodnewsinternational.com' ]
+    start_urls = []
     rules = ( Rule(LinkExtractor(), callback='parse_item' ),)
     # def parse_item(self,response):
         # self.logger.info('Hi, this is an item page! %s', response.url)
@@ -20,6 +21,12 @@ class TestSpider(CrawlSpider):
         # with open(filename, 'wb') as f:
              # f.write(response.body)
         # self.log('Saved file %s' % filename)
+    def __init__(self, *args, **kwargs):
+        super(TestSpider, self).__init__(*args, **kwargs)
+        with open("urllist.txt") as file:
+          for line in file:
+              line = line.strip() #or someother preprocessing
+              self.start_urls.append(line)
     def parse_item(self,response):
         self.logger.info('Hi, this is an item page! %s', response.url)
         page = int(round(time.time() * 1000))
